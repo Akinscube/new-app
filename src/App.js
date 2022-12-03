784feddc891156b8b41a033242dd83bf09cebf1e
Akinyemi Akinrodoye <akinyemi.akinlabi@rocketmail.com>
2022-12-03 01:27:39 +0100

    profile

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Profile from "./components/profile/profile";
+
+const handleName = (fullname) => {
+  alert(fullname);
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile
+        fullname={"Akinyemi"}
+        bio={"I am an aspiring Professional ReactJs Developer, possessing the required skills to work on your projects"}
+        profession={"ReactJs Developer"}
+        handleName={handleName}
+      >
+        <img
+          style={{ height: "100px", width: "100px"  ,objectFit: "cover", borderRadius: "100%" }}
+          src="https://media.istockphoto.com/id/613531310/photo/young-and-handsome.jpg?s=612x612&w=0&k=20&c=RJVt3sQQ6uhh9JPSBjgTnnKrYlexIJHLsYMHOr_Dskg="
+          alt="profile picture"
+        />
+      </Profile>
     </div>
   );
 }
